33eb3d6ee6f5ab6170937fd53c757a6281ee4b97
mrzhssss <mrzhssss@gmail.com>
2023-12-03 21:48:18 +0800

    update

--- src/main/java/com/zhs/common/controller/WmsLocTailController.java
+++ src/main/java/com/zhs/common/controller/WmsLocTailController.java
@@ -35,6 +35,40 @@ public class WmsLocTailController extends BaseController {
     @Autowired
     private EmailService emailService;
 
+    @RequestMapping("/notice")
+    public R noticeUsers(){
+        StringBuffer content = new StringBuffer();
+        List<WmsLocTail> locTails = locTailService.list(new QueryWrapper<WmsLocTail>()
+                .le("m_expired_date", new Date()));
+        List<User> users = usersService.list();
+
+        if (locTails.size() <= 0 || users.size() <= 0) {
+            return R.error("There are no user or no expired item");
+        }
+        content.append("<table border=\"1\">\n" +
+                "  <caption>\n" +
+                "    Expired Items\n" +
+                "  </caption>\n" +
+                "  <tr>\n" +
+                "    <th scope=\"col\" align=\"center\">Location Number</th>\n" +
+                "    <th scope=\"col\" align=\"center\">Material Name</th>\n" +
+                "    <th scope=\"col\" align=\"center\">Amount</th>\n" +
+                "    <th scope=\"col\" align=\"center\">Expired Date</th>\n" +
+                "  </tr>");
+        for (WmsLocTail wmsLocTail : locTails) {
+            content.append("<tr>\n" +
+                    "    <th scope=\"row\" align=\"center\">"+wmsLocTail.getLocNo()+"</th>\n" +
+                    "    <td align=\"center\">"+wmsLocTail.getName()+"</td>\n" +
+                    "    <td align=\"center\">"+wmsLocTail.getQty()+"</td>\n" +
+                    "    <td align=\"center\">"+wmsLocTail.getExpiredDate$()+"</td>\n" +
+                    "  </tr>");
+        }
+        content.append("</table>");
+        for (User user : users) {
+            emailService.sendEmail(user.getEmail(),"Hello, admin. here is some expired items",content.toString());
+        }
+        return R.ok("The system had sent all expired items to all users.");
+    }
     @GetMapping("/list")
     public R list(@RequestParam(required = false) Map<String, Object> params,
                   @RequestParam(required = false) ArrayList<String> columns) {
@@ -80,38 +114,5 @@ public class WmsLocTailController extends BaseController {
     }
 
 
-    @RequestMapping("/notice")
-    public R noticeUsers(){
-        StringBuffer content = new StringBuffer();
-        List<WmsLocTail> locTails = locTailService.list(new QueryWrapper<WmsLocTail>()
-                .le("m_expired_date", new Date()));
-        List<User> users = usersService.list();
 
-        if (locTails.size() <= 0 || users.size() <= 0) {
-            return R.error("There are no user or no expired item");
-        }
-        content.append("<table border=\"1\">\n" +
-                "  <caption>\n" +
-                "    Expired Items\n" +
-                "  </caption>\n" +
-                "  <tr>\n" +
-                "    <th scope=\"col\" align=\"center\">Location Number</th>\n" +
-                "    <th scope=\"col\" align=\"center\">Material Name</th>\n" +
-                "    <th scope=\"col\" align=\"center\">Amount</th>\n" +
-                "    <th scope=\"col\" align=\"center\">Expired Date</th>\n" +
-                "  </tr>");
-        for (WmsLocTail wmsLocTail : locTails) {
-            content.append("<tr>\n" +
-                    "    <th scope=\"row\" align=\"center\">"+wmsLocTail.getLocNo()+"</th>\n" +
-                    "    <td align=\"center\">"+wmsLocTail.getName()+"</td>\n" +
-                    "    <td align=\"center\">"+wmsLocTail.getQty()+"</td>\n" +
-                    "    <td align=\"center\">"+wmsLocTail.getExpiredDate$()+"</td>\n" +
-                    "  </tr>");
-        }
-        content.append("</table>");
-        for (User user : users) {
-            emailService.sendEmail(user.getEmail(),"Hello, admin. here is some expired items",content.toString());
-        }
-        return R.ok("The system had sent all expired items to all users.");
-    }
 }
